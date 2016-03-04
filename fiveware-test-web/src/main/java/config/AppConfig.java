c0193853dda2db41243fadc29daec4ecf689d6a3
Douglas Nunes <Douglas Nunes>
2016-03-04 20:34:29 -0300

    Base config sem XML

--- fiveware-test-web/src/main/java/config/AppConfig.java
+++ fiveware-test-web/src/main/java/config/AppConfig.java
@@ -1,8 +1,33 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
+@EnableWebMvc
 @Configuration
-public class AppConfig {
+@ComponentScan({ "org.fiveware.test" })
 
+public class AppConfig extends WebMvcConfigurerAdapter {
+	 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	 
 }
+	
